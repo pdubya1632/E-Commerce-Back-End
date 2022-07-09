4eed11c8868a5f1c4d247dbac9830a9e54d961eb
Patrick Richardson Wright <wrightmight@gmail.com>
2022-07-09 00:20:32 -0700

    add video link

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ UW Coding Bootcamp : Homework 11
 Starter back end for an e-commerce site. Using Express.js API and Sequelize to interact with a PostgreSQL database.
 
 ## Walkthrough Video
-Coming soon!
+[Click here](https://www.loom.com/share/04faa4bee07f486185d9878b5cc134df)
--- migrations/20220709025146-create-products.js
+++ migrations/20220709025146-create-products.js
@@ -38,6 +38,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Products');
-    await queryInterface.removeColumn('Product', 'category_id');
+    // await queryInterface.removeColumn('Products', 'category_id');
   },
 };
